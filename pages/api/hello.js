f5a26a67f58cf2c528d2f9610987c3ea88fb6882
s18010 <s18010@std.it-college.ac.jp>
2019-08-07 18:43:15 +0900

    文言変更

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 export default (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200;
-  res.end(JSON.stringify({ name: 'Nextjs' }));
+  res.end(JSON.stringify({ name: 'hi' }));
 };
